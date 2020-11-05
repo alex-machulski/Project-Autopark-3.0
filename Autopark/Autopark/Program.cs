1179dd2598fd3b5afc67c092a46ce15780ba5110
alex-machulski <aliaksandr.machulski@gmail.com>
2020-11-05 14:28:24 +0300

    level 1 done

---
+++ Autopark/Autopark/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace Autopark
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            VehicleType[] carTypes = new VehicleType[] 
+            { new VehicleType("Bus", 1.2), new VehicleType("Car"), new VehicleType("Rink", 1.5), new VehicleType("Tractor", 1.2) };
+            double maxTaxRate = 0.0, sumTaxRate = 0.0;
+
+            carTypes[carTypes.Length - 1].RoadTaxRate = 1.3;  
+            //print with Display(), finding max tax rate and sum of tax rates
+            foreach (VehicleType item in carTypes)
+            {
+                item.Display();
+                Console.WriteLine();
+                if (item.RoadTaxRate > maxTaxRate)
+                    maxTaxRate = item.RoadTaxRate;
+                sumTaxRate += item.RoadTaxRate;
+            }
+            double averageTaxRate = sumTaxRate / carTypes.Length;
+            Console.WriteLine($"Maximum tax rate - {maxTaxRate}");
+            Console.WriteLine($"Average tax rate - {averageTaxRate}");
+            //print with ToString()
+            foreach (VehicleType item in carTypes) 
+            {
+                Console.WriteLine(item);
+            }            
+        }
+    }
+
+    class VehicleType
+    {
+        public string CarType { get; set; }
+        public double RoadTaxRate { get; set; }
+
+        public VehicleType()
+        {
+        }
+
+        public VehicleType(string typeName, double taxCoefficient = 1)
+        {
+            CarType = typeName;
+            RoadTaxRate = taxCoefficient;
+        }       
+
+        public void Display() 
+        {
+            Console.WriteLine($"CarType = {CarType}");
+            Console.WriteLine($"RoadTaxRate = {RoadTaxRate}");
+        }
+
+        public override string ToString() => $"{CarType}, {RoadTaxRate}";
+        
+    }
+}
